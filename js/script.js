cfb49911321b1b8b3c614e3127d0ba0230680aaa
kopiUNG <kopiung19@gmail.com>
2024-04-27 12:30:59 +0800

    update swiper
    hapus js slider swiper di scipt.js

--- js/script.js
+++ js/script.js
@@ -18,24 +18,3 @@ document.querySelectorAll('.image-slider img').forEach(images => {
     };
 });
 
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    loop: true,
-    grabCursor: true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1
-        },
-        768: {
-            slidesPerView: 2
-        }
-    },
-});
