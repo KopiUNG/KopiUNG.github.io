34f57ab02547eef466a57120fe6dd46855ad5b7d
kopiUNG <kopiung19@gmail.com>
2024-08-17 18:40:43 +0800

    focus menu
    focus menu active

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
+let navbarhome = document.querySelector('.menu-home');
+let navbarabout = document.querySelector('.menu-about');
+let navbarmenu = document.querySelector('.menu-menu');
+let navbargalery = document.querySelector('.menu-galery');
+let navbarcontact = document.querySelector('.menu-contact');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
@@ -10,6 +15,46 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
+//focus
+
+navbarhome.onclick = () => {
+    navbarhome.classList.add('active');
+    navbarabout.classList.remove('active');
+    navbarmenu.classList.remove('active');
+    navbargalery.classList.remove('active');
+    navbarcontact.classList.remove('active');
+};
+navbarabout.onclick = () => {
+    navbarabout.classList.add('active');
+    navbarhome.classList.remove('active');
+    navbarmenu.classList.remove('active');
+    navbargalery.classList.remove('active');
+    navbarcontact.classList.remove('active');
+};
+navbarmenu.onclick = () => {
+    navbarmenu.classList.add('active');
+    navbarhome.classList.remove('active');
+    navbarabout.classList.remove('active');
+    navbargalery.classList.remove('active');
+    navbarcontact.classList.remove('active');
+};
+
+navbargalery.onclick = () => {
+    navbargalery.classList.add('active');
+    navbarhome.classList.remove('active');
+    navbarmenu.classList.remove('active');
+    navbarabout.classList.remove('active');
+    navbarcontact.classList.remove('active');
+};
+
+navbarcontact.onclick = () => {
+    navbarcontact.classList.add('active');
+    navbarhome.classList.remove('active');
+    navbarmenu.classList.remove('active');
+    navbargalery.classList.remove('active');
+    navbarabout.classList.remove('active');
+};
+
 
 document.querySelectorAll('.image-slider img').forEach(images => {
     images.onclick = () => {
